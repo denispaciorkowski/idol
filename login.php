b229b3610e24078de5d4d46d72092c2d8779b26e
Denis Paciorkowski <Denis@MacBook-Pro-van-Denis.local>
2015-05-20 14:09:24 +0200

    wat stijl aangepast

--- login.php
+++ login.php
@@ -27,6 +27,9 @@
 		?>
 	<div id="login_second_content">
 		<h1 id="logintitle">Vul hier je gegevens in om in te loggen</h1>
+		<p id="login_text">Om dingen te kunnen aanpassen en/of wijzigen dient
+			u een wachtwoord nodig te hebben, dit wachtwoord kunt u aanvragen/wijzigen
+			bij hogerbevoegden.</p>
 		<br>
 		<form action="loginpage.php" method="post">
 			<label for="wachtwoord">Wachtwoord</label>
